4ead264ed11bbb0ea50641feba0af4dc58961795
CHAURASIYA MAHADEV <mahadev.chaurasiya@CHAURASIYAs-MacBook-Pro.local>
2023-08-12 21:36:03 +0530

    css grid

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,10 @@
-var gulp = require('gulp');
-var browserSync = require('browser-sync').create();
-gulp.task('browser-sync', function(){
-    browserSync.init({
-        server:{
-            baseDir:"./"
-        }
-    });
-    gulp.watch(["./*.html","./css/*.css"]).on('change', browserSync.reload);
-})
+var gulp = require("gulp");
+var browserSync = require("browser-sync").create();
+gulp.task("browser-sync", function () {
+  browserSync.init({
+    server: {
+      baseDir: "./",
+    },
+  });
+  gulp.watch(["./*.html", "./css/*.css"]).on("change", browserSync.reload);
+});
